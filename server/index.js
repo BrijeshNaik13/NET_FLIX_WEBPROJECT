4f3ce2e84ec195e64c32f0a2b718e23ede928169
unknown <officialbrijesh.naik@gmail.com>
2026-02-22 01:42:21 +0530

    Fixed static file path

--- server/index.js
+++ server/index.js
@@ -60,10 +60,10 @@ app.get('/', (req, res) => {
 
 // Serve static files in production
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../dist')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        res.sendFile(path.join(__dirname, '../dist/index.html'));
     });
 }
 
